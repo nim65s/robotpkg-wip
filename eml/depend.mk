90013cb13b0605ad082ac73db62d44940f16e160
Matthieu Herrb <matthieu@herrb.eu>
2025-06-16 14:07:03 +0200

    [wip/eml] Add Ethercat Master Library version 1.8.15

---
+++ eml/depend.mk
@@ -0,0 +1,22 @@
+DEPEND_DEPTH:=		${DEPEND_DEPTH}+
+EML_DEPEND_MK:=		${EML_DEPEND_MK}+
+
+ifeq (+,${DEPEND_DEPTH})
+DEPEND_PKG+=		eml
+endif
+
+ifeq (+,${EML_DEPEND_MK})
+PREFER.eml?=		robotpkg
+
+DEPEN_USE+=		eml
+
+DEPEND_ABI.eml?=	eml>=1.8.15
+DEPEND_DIR.eml?=	../../wip/eml
+
+SYSTEM_SEARCH_eml=\
+	lib/libeml.so \
+	include/al/ethercat_AL.h \
+	share/eml/EMLConfig.cmake
+endif
+
+DEPEND_DEPTH:=	${DEPEND_DEPTH:+=}
